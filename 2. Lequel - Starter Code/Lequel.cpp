e06e55c74b1591c102dc607f3bbebefe178d8b32
ari <aschiaffino@itba.edu.ar>
2025-04-11 00:27:33 -0300

    fin

--- 2. Lequel - Starter Code/Lequel.cpp
+++ 2. Lequel - Starter Code/Lequel.cpp
@@ -1,7 +1,8 @@
 /**
  * @brief Lequel? language identification based on trigrams
  * @author Marc S. Ressl
- *
+ * @author Ariana Schiaffino
+ * @author Juan Manuel Sambucetti
  * @copyright Copyright (c) 2022-2023
  *
  * @cite https://towardsdatascience.com/understanding-cosine-similarity-and-its-application-fd42f585296a
@@ -35,7 +36,7 @@ TrigramProfile buildTrigramProfile(const Text &text)
     TrigramProfile trigramList;
     std::wstring trigramKey;
 
-    for(const auto& line : text) {                                   //line points to each element of text: a string
+    for(const auto& line : text) {                                   
 
         wstring unicodeString = converter.from_bytes(line);         //converts UTF - 8 string to wstring
 
@@ -46,7 +47,8 @@ TrigramProfile buildTrigramProfile(const Text &text)
 
             trigramKey = unicodeString.substr(n, 3);                //the current trigram is selected and saved
             string trigram = converter.to_bytes(trigramKey);        //convert wstring to UTF-8 string
-            trigramList[trigram]++;                                 //each time the trigram is repeated, its value increases. That's how each trigram's frequency is calculated.
+            trigramList[trigram]++;                                 //each time the trigram is repeated, its value increases. 
+                                                                    //That's how each trigram's frequency is calculated.
             trigramKey.clear();
             trigram.clear();
 
@@ -68,7 +70,7 @@ void normalizeTrigramProfile(TrigramProfile &trigramProfile)
 {
     float squareSumation = 0;
 
-    for(const auto& pair : trigramProfile) {            //pair points to each trigram with its value(frequency)
+    for(const auto& pair : trigramProfile) {            
         squareSumation += (pair.second * pair.second);   
     }
 
@@ -90,12 +92,12 @@ float getCosineSimilarity(TrigramProfile &textProfile, TrigramProfile &languageP
 {
     float simmilarity = 0;
 
-    for(auto& pair : textProfile) {                  //pair points to each element of textProfile: a trigram and its key
-                                                     //we did not opt for pair to point to each element of languageProfile
-                                                     //due to efficiency.More in ReadMe file.
-        auto it = languageProfile.find(pair.first); //it points to the element of languageProfile if
-                                                    //there was a match with an element of trigramProfile.
-        if(it != languageProfile.end()) {           //the condition means that a match was found
+    for(auto& pair : textProfile) {                         //we did not opt for pair to point to each element of languageProfile
+                                                            //due to efficiency.More in ReadMe file.
+                                                     
+        auto it = languageProfile.find(pair.first);         //it points to the element of languageProfile if
+                                                            //there was a match with an element of trigramProfile.
+        if(it != languageProfile.end()) {                   //the condition means that a match was found
             simmilarity += ((it->second) * pair.second);    
         }
     }
@@ -119,12 +121,12 @@ string identifyLanguage(const Text &text, LanguageProfiles &languages)
     std::string detectedLanguage;
     detectedLanguage.clear();
 
-    for(auto& dictionaries : languages) {            //dictionaries points to each Language object
+    for(auto& dictionaries : languages) {            
 
         float currentSimmilarity = getCosineSimilarity(ourTrigram, dictionaries.trigramProfile);
         
-        if(currentSimmilarity > maxSimmilarity) {   //the maximum value is obtained by comparing just with the last one
-                                                    //this is done for efficiency. More in the ReadMe file.
+        if(currentSimmilarity > maxSimmilarity) {           //the maximum value is obtained by comparing just with the last one
+                                                            //this is done for efficiency. More in the ReadMe file.
             maxSimmilarity = currentSimmilarity;
             detectedLanguage = dictionaries.languageCode;
         }
